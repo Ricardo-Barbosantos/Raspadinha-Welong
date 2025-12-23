f39fbd26e46f6bb12b71cce11c3ea4cd45505760
Ricardo-Barbosantos <ricardo.barbosanto@gmail.com>
2025-12-23 08:40:53 -0300

    fix: use dynamic base url for scratch card iframe

--- src/components/ScratchCardDialog.tsx
+++ src/components/ScratchCardDialog.tsx
@@ -13,7 +13,7 @@ export const ScratchCardDialog = ({ open, onOpenChange }: ScratchCardDialogProps
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-4xl w-[95vw] h-[90vh] p-0 border-none bg-transparent overflow-hidden">
         <iframe 
-          src="/RASPADINHA/index.html" 
+          src={`${import.meta.env.BASE_URL}RASPADINHA/index.html`} 
           className="w-full h-full border-none rounded-lg"
           title="Raspadinha"
         />
