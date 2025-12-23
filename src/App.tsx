99ba121f27bca99fd85be83ad3a209fd677741a9
Ricardo-Barbosantos <ricardo.barbosanto@gmail.com>
2025-12-23 12:16:54 -0300

    feat: config custom domain veraowelong.shop and update game sounds

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Raspadinha-Welong">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Page3 />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/Raspadinha-Welong/",
+  base: "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
