117bb2f696be6d50c42ba42ef9ae656a39de5a7e
Ricardo-Barbosantos <ricardo.barbosanto@gmail.com>
2025-12-23 08:35:26 -0300

    fix: add basename to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Raspadinha-Welong">
         <Routes>
           <Route path="/" element={<Page3 />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
